0c6c2211da640df203fbe8729be14e2e7edcf33e
pentagram5 <pentagram5@naver.com>
2024-11-29 07:06:24 +0900

    fix: redis docker setting 변경 - 암호

--- src/redis/redis.config.ts
+++ src/redis/redis.config.ts
@@ -1,38 +1,14 @@
 import { ConfigService } from '@nestjs/config';
+import { RedisSingleOptions } from '@nestjs-modules/ioredis';
 
-export const redisConfig = async (configService: ConfigService) => {
+export const redisConfig = (
+  configService: ConfigService,
+): RedisSingleOptions => {
   return {
-    type: 'cluster',
-    nodes: [
-      {
-        host: configService.get<string>('REDIS_HOST'),
-        port: 6371,
-      },
-      {
-        host: configService.get<string>('REDIS_HOST'),
-        port: 6372,
-      },
-      {
-        host: configService.get<string>('REDIS_HOST'),
-        port: 6373,
-      },
-      {
-        host: configService.get<string>('REDIS_HOST'),
-        port: 6374,
-      },
-      {
-        host: configService.get<string>('REDIS_HOST'),
-        port: 6375,
-      },
-      {
-        host: configService.get<string>('REDIS_HOST'),
-        port: 6379,
-      },
-    ],
+    type: 'single',
+    url: `redis://${configService.get<string>('REDIS_HOST')}:${configService.get<string>('REDIS_PORT')}`,
     options: {
-      redisOptions: {
-        password: 'bitnami',
-      },
+      password: configService.get<string>('REDIS_PASSWORD'),
     },
   };
 };
