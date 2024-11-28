9e8d6e4db889a4b69cc5826dfacb822c236488cc
pentagram5 <pentagram5@naver.com>
2024-11-29 07:06:13 +0900

    fix: redis docker setting 변경 - 암호

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,22 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TicketModule } from './ticket/ticket.module';
 import { RedisModule } from '@nestjs-modules/ioredis';
+import { ScheduleModule } from '@nestjs/schedule';
+import { redisConfig } from './redis/redis.config';
 
 @Module({
   imports: [
+    ScheduleModule.forRoot(),
     ConfigModule.forRoot({
       isGlobal: true,
       load: [],
       cache: true,
-      envFilePath: `.env.${process.env.NODE_ENV}`,
+      envFilePath: `.env`,
     }),
     RedisModule.forRootAsync({
-      useFactory: () => ({
-        type: 'single',
-        url: 'redis://localhost:6379',
-      }),
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => redisConfig(configService),
     }),
     TicketModule,
   ],
